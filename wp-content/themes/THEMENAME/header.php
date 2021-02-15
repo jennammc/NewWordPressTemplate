c0007b19ae5440e2decb22cdb4981f9807c253fb
Jenna M <jenna@dhxadv.com>
2021-02-15 08:05:44 -0800

    icons

--- wp-content/themes/THEMENAME/header.php
+++ wp-content/themes/THEMENAME/header.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- :::::::::: Fonts :::::::::: -->
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Roboto+Slab:700" rel="stylesheet"> 
 
     <!-- :::::::::: Critical Path CSS :::::::::: -->
