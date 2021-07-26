6a4f50e4ab0153efe1ba403c633febf204b7f03b
Jenna M <jenna@dhxadv.com>
2021-07-26 08:32:18 -0700

    update wordpress version and plugins

--- README.md
+++
@@ -1,5 +0,0 @@
-# WordPress Starter Template
-
-The idea is to create a template that we can pull down anytime we want to start a new WP site.  
-
-It could still use some work, but this is a good beginning.
--- wp-content/plugins/wp-migrate-db/wp-migrate-db.php
+++ wp-content/plugins/wp-migrate-db/wp-migrate-db.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: WP Migrate DB
-Plugin URI: http://wordpress.org/plugins/wp-migrate-db/
+Plugin URI: https://wordpress.org/plugins/wp-migrate-db/
 Description: Exports your database as a MySQL data dump (much like phpMyAdmin), does a find and replace on URLs and file paths, then allows you to save it to your computer.
 Author: Delicious Brains
-Version: 0.9.2
-Author URI: http://deliciousbrains.com
+Version: 2.0.2
+Author URI: https://deliciousbrains.com
 Network: True
 Text Domain: wp-migrate-db
 Domain Path: /languages/
@@ -22,76 +22,26 @@ Domain Path: /languages/
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // **********************************************************************
 
-$GLOBALS['wpmdb_meta']['wp-migrate-db']['version'] = '0.9.2';
+defined( 'ABSPATH' ) || exit;
 
-if ( ! class_exists( 'WPMDB_Utils' ) ) {
-	require dirname( __FILE__ ) . '/class/wpmdb-utils.php';
+$wpmdb_base_path                                   = dirname( __FILE__ );
+$GLOBALS['wpmdb_meta']['wp-migrate-db']['version'] = '2.0.2';
+
+$plugin_root = '/';
+
+if(!defined('WPMDB_VENDOR_DIR')){
+    define('WPMDB_VENDOR_DIR', __DIR__ . $plugin_root."vendor");
 }
 
-function wp_migrate_db_loaded() {
-	// exit quickly unless: standalone admin; one of our AJAX calls
-	if ( ! is_admin() || ( is_multisite() && ! current_user_can( 'manage_network_options' ) && ! WPMDB_Utils::is_ajax() ) ) {
-		return false;
-	}
-	wp_migrate_db();
+require WPMDB_VENDOR_DIR . '/autoload.php';
+
+require 'setup-plugin.php';
+
+if ( version_compare( PHP_VERSION, WPMDB_MINIMUM_PHP_VERSION, '>=' ) ) {
+	require_once $wpmdb_base_path . '/class/autoload.php';
+	require_once $wpmdb_base_path . '/setup-mdb.php';
 }
 
-add_action( 'plugins_loaded', 'wp_migrate_db_loaded' );
-
-/**
- * Populate the $wpmdb global with an instance of the WPMDB class and return it.
- *
- * @return WPMDB The one true global instance of the WPMDB class.
- */
-function wp_migrate_db() {
-	global $wpmdb;
-
-	if ( ! is_null( $wpmdb ) ) {
-		return $wpmdb;
-	}
-
-	$abspath = dirname( __FILE__ );
-
-	require_once $abspath . '/class/wpmdb-base.php';
-	require_once $abspath . '/class/wpmdb.php';
-	require_once $abspath . '/class/wpmdb-replace.php';
-	require_once $abspath . '/class/wpmdb-migration-state.php';
-	require_once $abspath . '/class/wpmdb-sanitize.php';
-
-	$wpmdb = new WPMDB( __FILE__ );
-
-	return $wpmdb;
+function wpmdb_remove_mu_plugin() {
+	do_action( 'wp_migrate_db_remove_compatibility_plugin' );
 }
-
-function wpmdb_cli_loaded() {
-	// register with wp-cli if it's running, and command hasn't already been defined elsewhere
-	if ( defined( 'WP_CLI' ) && WP_CLI && ! class_exists( 'WPMDB_Command' ) ) {
-		require_once dirname( __FILE__ ) . '/class/wpmdb-command.php';
-	}
-}
-add_action( 'plugins_loaded', 'wpmdb_cli_loaded', 20 );
-
-function wpmdb_cli() {
-	global $wpmdb_cli;
-
-	if ( ! is_null( $wpmdb_cli ) ) {
-		return $wpmdb_cli;
-	}
-
-	if ( function_exists( 'wp_migrate_db' ) ) {
-		wp_migrate_db();
-	} else {
-		return false;
-	}
-
-	do_action( 'wp_migrate_db_cli_before_load' );
-
-	require_once dirname( __FILE__ ) . '/class/wpmdb-cli.php';
-	$wpmdb_cli = new WPMDB_CLI( __FILE__ );
-
-	do_action( 'wp_migrate_db_cli_after_load' );
-
-	return $wpmdb_cli;
-}
-
-add_action( 'activated_plugin', array( 'WPMDB_Utils', 'deactivate_other_instances' ) );
